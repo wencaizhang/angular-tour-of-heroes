c5c7ffaf2ff3344f4204bc1e40ab8950535d1427
wencaizhang <1052642137@qq.com>
2020-09-16 17:34:34 +0800

    创建消息服务

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HeroService } from '../hero.service'
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,15 +14,16 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
-    console.log(hero)
+    console.log(hero);
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
     this.heroService.getHeroes()
-    .subscribe(heroes => this.heroes = heroes)
+    .subscribe(heroes => this.heroes = heroes);
   }
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
